413c4ef18d6b7f2fc13061cef3cf399c5d5feef0
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2025-09-18 16:44:34 +0100

    Take snapshot before and after mutations and on leaving record
    Actually at point of send (when an enter button is
    pressed) and when new data is received within person
    (detected by monitory query state and mutation state
    for change) and at point of departure from the record
    (provided this is done using buttons within the interface).
    
    Show a screenshot before send if the relevant option is
    selected in the menu bar.
    
    If we cannot take a screenshot, still download the raw data.

--- src/lasappbar.jsx
+++ src/lasappbar.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import { AppBar, IconButton, Menu, MenuItem, Toolbar, Typography } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import { usePrefs } from './prefs';
 
 //Menu implementation follows https://www.reddit.com/r/reactjs/comments/vhsijs/material_ui_open_a_single_dropdown_menu_instead/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
 export default function LaSAppBar() {
   const [menuAnchorEl, setMenuAnchorEl] = useState(null);
+  const prefs = usePrefs();
 
   return (
     <AppBar position='sticky'>
@@ -13,7 +15,7 @@ export default function LaSAppBar() {
           <MenuIcon onClick={(e) => {setMenuAnchorEl(e.currentTarget)}}/>
         </IconButton>
         <Menu anchorEl={menuAnchorEl} onClose={()=>setMenuAnchorEl(null)} onClick={()=>setMenuAnchorEl(null)} open={Boolean(menuAnchorEl)}>
-          <MenuItem>Dummy</MenuItem>
+          <MenuItem onClick={prefs.toggleScreenshot}>{prefs.screenshot ? "Disable screenshot preview" : "Enable screenshot preview"}</MenuItem>
         </Menu>
         <Typography variant='h6'>Lives at Sea</Typography>
       </Toolbar>
