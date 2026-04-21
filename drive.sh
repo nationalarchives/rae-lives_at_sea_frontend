ec6d5e83a0f495395f4e1b185527b57b0f037316
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2026-04-21 18:38:01 +0100

    Modify drive.sh for convenient dockerisation of convert.js
    drive.sh now runs in more stages, prepares files in a place that
    can be docker-accessible and generates an alternative output to
    make use of those files via a relative path.
    
    Use then becomes, roughly:
    cp -r las_fe a
    cd a
    npm i
    cd for_bucket
    cat rel_CHANGE_LOG | node ../convert.js 2> CONVERT_ERR_LOG | tee LOG
    
    On host:
    docker cp <container name from docker ps>:/app/a/for_bucket_1/ output

--- drive.sh
+++ drive.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+#sed line is for convenient running of the node step in a quite constrained docker
 
 targdir="$(dirname $(readlink -f ${BASH_SOURCE}))"
-rm "${targdir}"/for_bucket/* && \
-cd "${targdir}"/for_bucket/ && \
-../changes.py ~/c_home/Downloads/SNAPSHOTS/for_sp/202* 2> CHANGE_ERR_LOG | node ../convert.js 2> CONVERT_ERR_LOG | tee LOG
+echo $targdir
+rm -f "${targdir}"/for_bucket_1/*LOG && \
+cd "${targdir}"/for_bucket_1/ && \
+rm -rf snapshots/ && \
+mkdir snapshots/ && \
+cp -r ../../las_utils/rehydration/snapshots/*/ snapshots/ && \
+../changes.py snapshots/*/ 2> CHANGE_ERR_LOG > CHANGE_LOG && \
+sed "s@^\([[:digit:]]\+:\(service\|name\):[[:digit:]_-]\+:\)${targdir}/for_bucket_1/\(.*\)@\1\3@" CHANGE_LOG > rel_CHANGE_LOG && \
+cat CHANGE_LOG | node ../convert.js 2> CONVERT_ERR_LOG | tee LOG
